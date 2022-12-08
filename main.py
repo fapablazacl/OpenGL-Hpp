086ae11c0673e634da59e55ed5a3aaa725d3eedb
Felipe Apablaza <felipe.apablaza@protonmail.com>
2022-12-08 12:44:38 -0300

    now generated code is stored in a file

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 
 import xml.dom.minidom
-
+import sys
 import glregistry
 import hppgenerator
+import os
 
-def main():
+def generate_hpp_header_filename(api, version):
+    return api + version.replace(".", "") + ".hpp"
+
+def generate_hpp_header(gl_xml_file_path, api, version, output_folder):
+    print("generating header file, with api " + api + " and version " + version)
+
     # Open XML document using minidom parser
-    DOMTree = xml.dom.minidom.parse("OpenGL-Registry/xml/gl.xml")
+    DOMTree = xml.dom.minidom.parse(gl_xml_file_path)
     parser = glregistry.GLXMLParser()
     repository = parser.create_repository(DOMTree)
     generator = hppgenerator.CodeGenerator(repository)
-    consolidated_require = repository.consolidate("gl", "3.3")
+    consolidated_require = repository.consolidate(api, version)
     generated_code = generator.generate_consolidated_require(consolidated_require)
-    print(generated_code)
+
+    output_filename = os.path.join(output_folder, generate_hpp_header_filename(api, version))
+
+    file = open(output_filename, "w")
+    file.write(generated_code)
+    file.close()
 
     """
     classdict = consolidate_classes(commands)
@@ -24,5 +35,10 @@ def main():
         generate_class(classdict[key], key)
     """
 
+def main():
+    print(sys.argv)
+    generate_hpp_header("OpenGL-Registry/xml/gl.xml", "gl", "3.3", "tests/test-gl10")
+    
+    
 if __name__ == "__main__":
     main()
